df2f83d1c2e175eeccaa337224fa210cfbd0dad7
Amaitari Hezekiah Philip <amaitarihezekiah@gmail.com>
2019-02-21 16:34:05 +0100

    Updated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 $("#year").text(new Date().getFullYear());
+
+$(document).ready(function() {
+  $(".showTrailer").on("click", function(event) {
+    event.preventDefault();
+    let url = $(this).attr("href");
+    console.log(url);
+    $("#trailerVideo").attr("src", url);
+    $("#trailerModal").modal("show");
+  });
+
+  /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed again */
+  // $("#trailerModal").on("show.bs.modal", function() {
+  //   $("#trailerVideo").attr("src", url);
+  // });
+
+  /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+  $("#trailerModal").on("hide.bs.modal", function() {
+    $("#trailerVideo").attr("src", "");
+  });
+});
